ed50d8855820b465c4a52c3b3ad422c2b124b4d9
GauravPawar101 <GauravPawar2522@gmail.com>
2025-04-05 21:09:55 +0530

    all_done

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+// packages/db/index.ts
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['error', 'warn'], // optional
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
